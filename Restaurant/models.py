2a5c47624d7ae73e3fc1535efbcc0dcfea2271ef
kalkidan Addisu <jcskal15@gmail.com>
2024-06-24 17:13:03 +0300

    edit the varible names

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class menu(models.Model):
     ID = models.IntegerField( primary_key=True)
-    Title = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField()
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
